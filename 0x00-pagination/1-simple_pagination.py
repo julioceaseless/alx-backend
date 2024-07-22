b95fbbe34bcdcf28ec7f6cf1055269b3d91a8e61
julioceaseless <julioceaseless@gmail.com>
2024-07-22 06:19:20 +0000

    ignore

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Simple pagination
+"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Determines the start and end index based on the pagination
+        parameters
+        """
+        assert type(page) == int
+        assert (page > 0)
+        assert type(page_size) == int
+        assert (page_size > 0)
+
+        end_index = page_size * page
+        start_index = end_index - page_size
+        if end_index > len(self.dataset()):
+            return []
+        data = self.dataset()
+        return data[start_index:end_index]
