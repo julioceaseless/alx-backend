6265ad46dd966b43962551ad8be998bebbe9615e
julioceaseless <julioceaseless@gmail.com>
2024-07-24 17:43:07 +0000

    first commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Basic caching
+    """
+    def put(self, key, item):
+        """
+        Add data to cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+        else:
+            pass
+
+    def get(self, key):
+        """
+        Get data from cache
+        """
+        if key:
+            return self.cache_data.get(key)
+        return None
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Create a FIFO cache
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFO caching class
+    """
+    def __init__(self):
+        """
+        initialize instance
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add data to cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                cache_keys = list(self.cache_data.keys())
+                print("DISCARD: {}".format(self.cache_data.pop(cache_keys[0])))
+        else:
+            pass
+
+    def get(self, key):
+        """
+        Get data from cache
+        """
+        if key:
+            return self.cache_data.get(key)
+        return None
