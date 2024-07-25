07de418db7929f0ddb4c3c654837e0a121165270
julioceaseless <julioceaseless@gmail.com>
2024-07-25 02:14:35 +0000

    4-mru_cache.py

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Create caching mechanism using MRUCache Algorithm
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Last In First Out caching class
+    """
+    def __init__(self):
+        """
+        initialize
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        Add data to cache
+        """
+        # Add item to cache
+        if key and item:
+            if key in self.cache_data:
+                # Update the existing item and move the key to the end
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # remove the least recently used key
+                mru_key = self.order.pop()
+
+                popped_item = self.cache_data.pop(mru_key)
+
+                print("DISCARD: {}".format(mru_key))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """
+        Get data from cache
+        """
+        if key and key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
