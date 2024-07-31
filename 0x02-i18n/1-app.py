b095afa24b3d4916bbe475cb05420b59c4acdc92
julioceaseless <julioceaseless@gmail.com>
2024-07-31 08:22:34 +0300

    Add babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,11 +8,27 @@ from flask_babel import Babel
 # create a flask instance
 app = Flask(__name__)
 
+# instantiate babel
+babel = Babel(app)
+
+
+# create configuration object
+class Config:
+    """class to configure languages in the app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# configure flask app
+app.config.from_object(Config)
+
+
 # route for index page
 @app.route("/")
 def index():
     """index page"""
-    return render_template('1-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
