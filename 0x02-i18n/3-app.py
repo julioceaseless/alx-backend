41ce9b895f58a6200c3b41066d0722f6dea875f7
julioceaseless <julioceaseless@gmail.com>
2024-08-03 05:58:44 +0300

    pass locale as a url parameter

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python3
 """Flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 # create configuration object
 class Config:
     """class to configure languages in the app"""
-    LANGUAGES = ["en", "fr"]
+    LANGUAGES = ["de", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 # language selector
-def get_locale():
+def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# create a flask instance
 app = Flask(__name__)
-
-# Add configurations to flask app
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 
-# instantiate babel
+
 babel = Babel(app, locale_selector=get_locale)
 
 
 # route for index page
 @app.route("/")
-def index():
+def index() -> str:
     """index page"""
-    print(request.headers)
     return render_template('3-index.html')
 
 
 if __name__ == "__main__":
     """run app when executed directly"""
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
