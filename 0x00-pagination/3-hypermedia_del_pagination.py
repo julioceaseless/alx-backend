f46d03c1ecd7f250de5a49c466e62de79f5e7351
julioceaseless <julioceaseless@gmail.com>
2024-08-06 19:52:11 +0300

    pep8

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 class Server:
@@ -45,7 +45,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> dict:
+                        page_size: int = 10) -> Dict[int, List]:
         """
         Returns a dictionary with pagination details considering deletions.
         """
