750ce6cbea6d8153388acaf841c9bc0557701487
julioceaseless <julioceaseless@gmail.com>
2024-08-28 11:32:03 +0300

    make the code lean

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,23 +1,13 @@
 import { createClient } from 'redis';
 
-async function main() {
-    // Create an asynchronous Redis client
-    const client = await createClient()
-        .on('error', (err) => console.log('Redis client not connected to the server', err))
-        .once('connect', () => console.log('Redis client connected to the server'))
-        .connect
+// create a redis client instance
+const client = createClient();
 
-    // Connect the client
-    await client.connect();
-
-    // Set a key-value pair in Redis
-    await client.set('key', 'value');
-
-    // Get the value associated with the key
-    const value = await client.get('key');
-    console.log('Retrieved value:', value);
-
-    // Disconnect the client
-    await client.disconnect();
-}
-main().catch(console.error);
+// catch connection error
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server', err);
+});
+// display message on successful connection to server
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
